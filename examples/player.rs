22ef221385d4221c7307461d804a44a3c6b3f6e5
Andrew Fuchs <andrew-fuchs@users.noreply.github.com>
2021-04-01 17:26:09 -0400

    Added more primitive tags to `player` example

--- examples/player.rs
+++ examples/player.rs
@@ -8,6 +8,7 @@ use std::fs::File;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Player {
+    // player related tags
     #[serde(rename = "DataVersion")]
     data_version: i32,
     #[serde(rename = "playerGameType")]
@@ -57,6 +58,66 @@ struct Player {
     #[serde(rename = "seenCredits")]
     seen_credits: bool,
     // recipe_book
+
+    // mob related tags
+    #[serde(rename = "AbsorptionAmount")]
+    absorption_amount: f32,
+    // active_effects
+    // attributes
+    // brain
+    // death_loot_table: Option<>
+    #[serde(rename = "DeathLootTableSeed")]
+    death_loot_table_seed: Option<i64>,
+    #[serde(rename = "DeathTime")]
+    death_time: i16,
+    #[serde(rename = "FallFlying")]
+    fall_flying: bool,
+    #[serde(rename = "Health")]
+    health: f32,
+    #[serde(rename = "HurtByTimestamp")]
+    hurt_by_timestamp: i32,
+    #[serde(rename = "HurtTime")]
+    hurt_time: i16,
+    // hand_items
+    // #[serde(rename = "LeftHanded")]
+    // left_handed: bool,
+    // #[serde(rename = "NoAI")]
+    // no_ai: bool,
+    #[serde(rename = "SleepingX")]
+    sleeping_x: Option<i32>,
+    #[serde(rename = "SleepingY")]
+    sleeping_y: Option<i32>,
+    #[serde(rename = "SleepingZ")]
+    sleeping_z: Option<i32>,
+    // team
+    // #[serde(rename = "TicksFrozen")]
+    // ticks_frozen: i32,
+
+    // entity related tags
+    #[serde(rename = "Air")]
+    air: i16,
+    #[serde(rename = "FallDistance")]
+    fall_distance: f32,
+    #[serde(rename = "Fire")]
+    fire: i16,
+    // #[serde(rename = "Glowing")]
+    // glowing: bool,
+    #[serde(rename = "Invulnerable")]
+    invulnerable: bool,
+    // motion:
+    // #[serde(rename = "NoGravity")]
+    // no_gravity: bool,
+    #[serde(rename = "OnGround")]
+    on_ground: bool,
+    // passengers
+    #[serde(rename = "PortalCooldown")]
+    portal_cooldown: i32,
+    // pos
+    // rotation
+    #[serde(rename = "Silent")]
+    silent: Option<bool>,
+    // tags
+    // uuid
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
